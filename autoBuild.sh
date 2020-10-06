457de9fcfb3b1458339ca48988a282c407b1c3e8
lxf <lxfljw@gmail.com>
2020-10-06 12:18:14 +0800

    feat✨ 更改shell

--- autoBuild.sh
+++ autoBuild.sh
@@ -2,7 +2,7 @@
  # @Author: luxiaofeng
  # @Date: 2020-09-24 21:26:00
  # @LastEditors: luxiaofeng
- # @LastEditTime: 2020-09-26 10:59:07
+ # @LastEditTime: 2020-10-06 12:17:47
  # @Description: file content
 ### 
 
@@ -15,9 +15,8 @@
 # cd ./admin/static
 # cnpm i
 # npm start
-cd ../blog/admin/server
 git pull
-sudo docker container rm admin-server
-sudo docker image rm admin-server
-sudo docker image build -t admin-server .
+# sudo docker container rm admin-server
+# sudo docker image rm admin-server
+sudo docker image build -t admin-server ../blog/admin/server
 sudo docker container run -p 8000:3000 -it admin-server
